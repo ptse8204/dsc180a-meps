2c135df28895043fa59c2ff0a7f6a95a66d9509e
g2su <57232020+g2su@users.noreply.github.com>
2022-12-05 23:16:55 -0800

    Add files via upload

--- run.py
+++ run.py
@@ -53,6 +53,8 @@ from aif360.datasets import MEPSDataset21
 
 from subprocess import getoutput
 
+from collections import defaultdict
+
 import gdown
 import zipfile
 
@@ -60,6 +62,94 @@ if __name__ == "__main__":
     all()
 
 def all():
+    ### output basic information of the train, valid, and test dataset
+    def describe(train=None, val=None, test=None):
+        if train is not None:
+           print("#### Training Dataset shape")
+           print(train.features.shape)
+        if val is not None:
+           print("#### Validation Dataset shape")
+           print(val.features.shape)
+        if test is not None:
+           print("#### Test Dataset shape")
+           print(test.features.shape)
+           print("#### Favorable and unfavorable labels")
+           print(test.favorable_label, test.unfavorable_label)
+           print("#### Protected attribute names")
+           print(test.protected_attribute_names)
+           print("#### Privileged and unprivileged protected attribute values")
+           print(test.privileged_protected_attributes,
+                 test.unprivileged_protected_attributes)
+           print("#### Dataset feature names")
+           print(test.feature_names)
+
+
+    def test(dataset, model, thresh_arr):
+       try:
+           # sklearn classifier
+           y_val_pred_prob = model.predict_proba(dataset.features)
+           pos_ind = np.where(model.classes_ == dataset.favorable_label)[0][0]
+       except AttributeError:
+           # aif360 inprocessing algorithm
+           y_val_pred_prob = model.predict(dataset).scores
+           pos_ind = 0
+
+       metric_arrs = defaultdict(list)
+       for thresh in thresh_arr:
+           y_val_pred = (y_val_pred_prob[:, pos_ind] > thresh).astype(np.float64)
+
+           dataset_pred = dataset.copy()
+           dataset_pred.labels = y_val_pred
+           metric = ClassificationMetric(
+                   dataset, dataset_pred,
+                   unprivileged_groups=unprivileged_groups,
+                   privileged_groups=privileged_groups)
+
+           metric_arrs['bal_acc'].append((metric.true_positive_rate()
+                                        + metric.true_negative_rate()) / 2)
+           metric_arrs['avg_odds_diff'].append(metric.average_odds_difference())
+           metric_arrs['disp_imp'].append(metric.disparate_impact())
+           metric_arrs['stat_par_diff'].append(metric.statistical_parity_difference())
+           metric_arrs['eq_opp_diff'].append(metric.equal_opportunity_difference())
+           metric_arrs['theil_ind'].append(metric.theil_index())
+
+       return metric_arrs
+
+    def plot(x, x_name, y_left, y_left_name, y_right, y_right_name):
+       fig, ax1 = plt.subplots(figsize=(10,7))
+       ax1.plot(x, y_left)
+       ax1.set_xlabel(x_name, fontsize=16, fontweight='bold')
+       ax1.set_ylabel(y_left_name, color='b', fontsize=16, fontweight='bold')
+       ax1.xaxis.set_tick_params(labelsize=14)
+       ax1.yaxis.set_tick_params(labelsize=14)
+       ax1.set_ylim(0.5, 0.8)
+
+       ax2 = ax1.twinx()
+       ax2.plot(x, y_right, color='r')
+       ax2.set_ylabel(y_right_name, color='r', fontsize=16, fontweight='bold')
+       if 'DI' in y_right_name:
+           ax2.set_ylim(0., 0.7)
+       else:
+           ax2.set_ylim(-0.25, 0.1)
+
+       best_ind = np.argmax(y_left)
+       ax2.axvline(np.array(x)[best_ind], color='k', linestyle=':')
+       ax2.yaxis.set_tick_params(labelsize=14)
+       ax2.grid(True)
+
+    def describe_metrics(metrics, thresh_arr):
+       best_ind = np.argmax(metrics['bal_acc'])
+       print("Threshold corresponding to Best balanced accuracy: {:6.4f}".format(thresh_arr[best_ind]))
+       print("Best balanced accuracy: {:6.4f}".format(metrics['bal_acc'][best_ind]))
+    #     disp_imp_at_best_ind = np.abs(1 - np.array(metrics['disp_imp']))[best_ind]
+       disp_imp_at_best_ind = 1 - min(metrics['disp_imp'][best_ind], 1/metrics['disp_imp'][best_ind])
+       print("Corresponding 1-min(DI, 1/DI) value: {:6.4f}".format(disp_imp_at_best_ind))
+       print("Corresponding average odds difference value: {:6.4f}".format(metrics['avg_odds_diff'][best_ind]))
+       print("Corresponding statistical parity difference value: {:6.4f}".format(metrics['stat_par_diff'][best_ind]))
+       print("Corresponding equal opportunity difference value: {:6.4f}".format(metrics['eq_opp_diff'][best_ind]))
+       print("Corresponding Theil index value: {:6.4f}".format(metrics['theil_ind'][best_ind]))
+
+
     np.random.seed(1)
     
     gdown.download("https://drive.google.com/uc?export=download&id=1YCJVsfkOxrUoaTRLfe59phWBIQO8mG3B", "./", quiet=False)
@@ -100,42 +190,42 @@ def all():
     rf_transf_metrics = reweighing_RF.testing_model(dataset_orig_panel19_val, dataset_orig_panel19_test, reweighing_RF.training_model(reweighing_LR.transform(reweighing_LR.create_dataset(dataset_orig_panel19_train), unprivileged_groups, privileged_groups)), unprivileged_groups, privileged_groups)
     
     #Prejudice Remover
-    model = PrejudiceRemover(sensitive_attr=sens_attr, eta=25.0)
-    pr_orig_scaler = StandardScaler()
-
-    dataset = dataset_orig_panel19_train.copy()
-    dataset.features = pr_orig_scaler.fit_transform(dataset.features)
-
-    pr_orig_panel19 = model.fit(dataset)
-    
-    thresh_arr = np.linspace(0.01, 0.50, 50)
-
-    dataset = dataset_orig_panel19_val.copy()
-    dataset.features = pr_orig_scaler.transform(dataset.features)
-
-    val_metrics = test(dataset=dataset,
-                       model=pr_orig_panel19,
-                       thresh_arr=thresh_arr)
-                       
-    pr_orig_best_ind = np.argmax(val_metrics['bal_acc'])
-    
-    
-    disp_imp = np.array(val_metrics['disp_imp'])
-    disp_imp_err = 1 - np.minimum(disp_imp, 1/disp_imp)
-    plot(thresh_arr, 'Classification Thresholds',
-         val_metrics['bal_acc'], 'Balanced Accuracy',
-         disp_imp_err, '1 - min(DI, 1/DI)')
-         
-    plot(thresh_arr, 'Classification Thresholds',
-    val_metrics['bal_acc'], 'Balanced Accuracy',
-    val_metrics['avg_odds_diff'], 'avg. odds diff.')
-    
-    dataset = dataset_orig_panel19_test.copy()
-    dataset.features = pr_orig_scaler.transform(dataset.features)
-
-    pr_orig_metrics = test(dataset=dataset,
-                           model=pr_orig_panel19,
-                           thresh_arr=[thresh_arr[pr_orig_best_ind]])
+#    model = PrejudiceRemover(sensitive_attr=sens_attr, eta=25.0)
+#    pr_orig_scaler = StandardScaler()
+#
+#    dataset = dataset_orig_panel19_train.copy()
+#    dataset.features = pr_orig_scaler.fit_transform(dataset.features)
+#
+#    pr_orig_panel19 = model.fit(dataset)
+#
+#    thresh_arr = np.linspace(0.01, 0.50, 50)
+#
+#    dataset = dataset_orig_panel19_val.copy()
+#    dataset.features = pr_orig_scaler.transform(dataset.features)
+#
+#    val_metrics = test(dataset=dataset,
+#                       model=pr_orig_panel19,
+#                       thresh_arr=thresh_arr)
+#
+#    pr_orig_best_ind = np.argmax(val_metrics['bal_acc'])
+#
+#
+#    disp_imp = np.array(val_metrics['disp_imp'])
+#    disp_imp_err = 1 - np.minimum(disp_imp, 1/disp_imp)
+#    plot(thresh_arr, 'Classification Thresholds',
+#         val_metrics['bal_acc'], 'Balanced Accuracy',
+#         disp_imp_err, '1 - min(DI, 1/DI)')
+#
+#    plot(thresh_arr, 'Classification Thresholds',
+#    val_metrics['bal_acc'], 'Balanced Accuracy',
+#    val_metrics['avg_odds_diff'], 'avg. odds diff.')
+#
+#    dataset = dataset_orig_panel19_test.copy()
+#    dataset.features = pr_orig_scaler.transform(dataset.features)
+#
+#    pr_orig_metrics = test(dataset=dataset,
+#                           model=pr_orig_panel19,
+#                           thresh_arr=[thresh_arr[pr_orig_best_ind]])
     
     #Logistic Regression w/ROC
     def compute_metrics(dataset_true, dataset_pred,
@@ -237,11 +327,9 @@ def all():
     
     dataset = dataset_orig_panel19_test.copy()
     thresh_arr = np.linspace(0.01, 1.0, 50)
-    roc_orig_best_ind = np.argmax(val_metrics['bal_acc'])
+    roc_orig_best_ind = 0
     
-    roc_orig_metrics = test(dataset,
-        model=ROC_model,
-        thresh_arr=[thresh_arr[roc_orig_best_ind]])
+    roc_orig_metrics = test(dataset,model=ROC_model,thresh_arr=[thresh_arr[roc_orig_best_ind]])
     
     x = ['Original']
     x2=["Post-processed"]
@@ -339,7 +427,7 @@ def all():
     
     dataset = dataset_orig_panel19_test.copy()
     thresh_arr = np.linspace(0.01, 1.0, 50)
-    roc_orig_best_ind = np.argmax(val_metrics['bal_acc'])
+    roc_orig_best_ind = 0
     
     roc_orig_metrics = test(dataset,
         model=ROC_model,
@@ -347,7 +435,7 @@ def all():
     
     dataset = dataset_orig_panel19_test.copy()
     thresh_arr = np.linspace(0.01, 1.0, 50)
-    roc_orig_best_ind = np.argmax(val_metrics['bal_acc'])
+    roc_orig_best_ind = 0
     
     roc_orig_metrics = test(dataset,
         model=ROC_model,
@@ -378,105 +466,17 @@ def all():
     lr_metric_test_df.columns=["bal_acc","avg_odds_diff","disp_imp","stat_par_diff","eq_opp_diff","theil_ind"]
     rf_metric_test_df=pd.DataFrame(rf_metric_test_aft,index=[0])[["Balanced accuracy","Average odds difference","Disparate impact","Statistical parity difference","Equal opportunity difference","Theil index"]]
     rf_metric_test_df.columns=["bal_acc","avg_odds_diff","disp_imp","stat_par_diff","eq_opp_diff","theil_ind"]
-    list_of_rows=[pd.DataFrame(lr_orig_metrics),pd.DataFrame(rf_orig_metrics),pd.DataFrame(lr_transf_metrics),
-                  pd.DataFrame(rf_transf_metrics),pd.DataFrame(pr_orig_metrics),lr_metric_test_df,rf_metric_test_df]
+#    list_of_rows=[pd.DataFrame(lr_orig_metrics),pd.DataFrame(rf_orig_metrics),pd.DataFrame(lr_transf_metrics),
+#                  pd.DataFrame(rf_transf_metrics),pd.DataFrame(pr_orig_metrics),lr_metric_test_df,rf_metric_test_df]
+    list_of_rows=[pd.DataFrame(lr_orig_metrics), pd.DataFrame(rf_orig_metrics), pd.DataFrame(lr_transf_metrics),
+                  pd.DataFrame(rf_transf_metrics), lr_metric_test_df, rf_metric_test_df]
     results_table=pd.concat(list_of_rows)
-    results_table["Classification"]=["Logistic Regression","Random Forest","Logistic Regression","Random Forest",
-                                    " ","Logistic Regression","Random Forest"]
-    results_table["Stage"]=["Original","Original","Pre-processing","Pre-processing","In-processing","Post-processing","Post-processing"]
-    results_table["Bias Mitigator"]=[" "," ","Reweighing","Reweighing","Prejudice Remover","Reject Option","Reject Option"]
+    results_table["Classification"]=["Logistic Regression","Random Forest","Logistic Regression","Random Forest","Logistic Regression"] #"Random Forest"]
+    results_table["Stage"]=["Original","Original","Pre-processing","Pre-processing","Post-processing"]#"Post-processing"]
+    results_table["Bias Mitigator"]=[" "," ","Reweighing","Reweighing","Reject Option"] #"Reject Option"]
     results_table=results_table.set_index(["Stage","Classification","Bias Mitigator"])
     print(results_table)
     
-    ### output basic information of the train, valid, and test dataset
-    def describe(train=None, val=None, test=None):
-        if train is not None:
-            print("#### Training Dataset shape")
-            print(train.features.shape)
-        if val is not None:
-            print("#### Validation Dataset shape")
-            print(val.features.shape)
-
-        if test is not None:
-            print("#### Test Dataset shape")
-            print(test.features.shape)
-            print("#### Favorable and unfavorable labels")
-            print(test.favorable_label, test.unfavorable_label)
-            print("#### Protected attribute names")
-            print(test.protected_attribute_names)
-            print("#### Privileged and unprivileged protected attribute values")
-            print(test.privileged_protected_attributes,
-                  test.unprivileged_protected_attributes)
-            print("#### Dataset feature names")
-            print(test.feature_names)
-
-
-    def test(dataset, model, thresh_arr):
-        try:
-            # sklearn classifier
-            y_val_pred_prob = model.predict_proba(dataset.features)
-            pos_ind = np.where(model.classes_ == dataset.favorable_label)[0][0]
-        except AttributeError:
-            # aif360 inprocessing algorithm
-            y_val_pred_prob = model.predict(dataset).scores
-            pos_ind = 0
-
-        metric_arrs = defaultdict(list)
-        for thresh in thresh_arr:
-            y_val_pred = (y_val_pred_prob[:, pos_ind] > thresh).astype(np.float64)
-
-            dataset_pred = dataset.copy()
-            dataset_pred.labels = y_val_pred
-            metric = ClassificationMetric(
-                    dataset, dataset_pred,
-                    unprivileged_groups=unprivileged_groups,
-                    privileged_groups=privileged_groups)
-
-            metric_arrs['bal_acc'].append((metric.true_positive_rate()
-                                         + metric.true_negative_rate()) / 2)
-            metric_arrs['avg_odds_diff'].append(metric.average_odds_difference())
-            metric_arrs['disp_imp'].append(metric.disparate_impact())
-            metric_arrs['stat_par_diff'].append(metric.statistical_parity_difference())
-            metric_arrs['eq_opp_diff'].append(metric.equal_opportunity_difference())
-            metric_arrs['theil_ind'].append(metric.theil_index())
-
-        return metric_arrs
-
-    def plot(x, x_name, y_left, y_left_name, y_right, y_right_name):
-        fig, ax1 = plt.subplots(figsize=(10,7))
-        ax1.plot(x, y_left)
-        ax1.set_xlabel(x_name, fontsize=16, fontweight='bold')
-        ax1.set_ylabel(y_left_name, color='b', fontsize=16, fontweight='bold')
-        ax1.xaxis.set_tick_params(labelsize=14)
-        ax1.yaxis.set_tick_params(labelsize=14)
-        ax1.set_ylim(0.5, 0.8)
-
-        ax2 = ax1.twinx()
-        ax2.plot(x, y_right, color='r')
-        ax2.set_ylabel(y_right_name, color='r', fontsize=16, fontweight='bold')
-        if 'DI' in y_right_name:
-            ax2.set_ylim(0., 0.7)
-        else:
-            ax2.set_ylim(-0.25, 0.1)
-
-        best_ind = np.argmax(y_left)
-        ax2.axvline(np.array(x)[best_ind], color='k', linestyle=':')
-        ax2.yaxis.set_tick_params(labelsize=14)
-        ax2.grid(True)
-
-    def describe_metrics(metrics, thresh_arr):
-        best_ind = np.argmax(metrics['bal_acc'])
-        print("Threshold corresponding to Best balanced accuracy: {:6.4f}".format(thresh_arr[best_ind]))
-        print("Best balanced accuracy: {:6.4f}".format(metrics['bal_acc'][best_ind]))
-    #     disp_imp_at_best_ind = np.abs(1 - np.array(metrics['disp_imp']))[best_ind]
-        disp_imp_at_best_ind = 1 - min(metrics['disp_imp'][best_ind], 1/metrics['disp_imp'][best_ind])
-        print("Corresponding 1-min(DI, 1/DI) value: {:6.4f}".format(disp_imp_at_best_ind))
-        print("Corresponding average odds difference value: {:6.4f}".format(metrics['avg_odds_diff'][best_ind]))
-        print("Corresponding statistical parity difference value: {:6.4f}".format(metrics['stat_par_diff'][best_ind]))
-        print("Corresponding equal opportunity difference value: {:6.4f}".format(metrics['eq_opp_diff'][best_ind]))
-        print("Corresponding Theil index value: {:6.4f}".format(metrics['theil_ind'][best_ind]))
-
-
 
 #def main():
 #    eda.main()
